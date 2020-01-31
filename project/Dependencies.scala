d740268c16da9f4980ef9a43041b5ed2826cf571
Scala Steward <me@scala-steward.org>
2020-01-31 12:26:45 +0100

    Update tapir-akka-http-server, tapir-core, ... to 0.12.19

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -249,7 +249,7 @@ object Dependencies {
     val scalaTest = "3.1.0"
     val specs2 = "4.8.3"
     val slick = "3.3.2"
-    val tapir = "0.12.17"
+    val tapir = "0.12.19"
     val swaggerUi = "3.24.3"
     val testcontainers = "0.35.0"
     val testcontainersKafka = "1.12.5"
